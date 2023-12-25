92efe7469b633cd8787aba79140c2650c8efae50
ishubhchoudhary <shubhamchoudhary55555@gmail.com>
2023-12-25 12:38:18 +0530

    first commit

---
+++ app/Http/Controllers/DemoController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DemoController extends Controller
+{
+    public function index() {
+        $array = [
+            [
+                'name' => 'Shubham choudhary',
+                'email' => 'shubham@gmail.com'
+            ],
+            [
+                'name' => 'Sanjay choudhary',
+                'email' => 'sanjay@gmail.com'
+            ]
+        ];
+
+        return response()->json([
+            'message' => '2 Users found',
+            'data' => $array,
+            'status' => true
+        ], 200);
+    }
+}
